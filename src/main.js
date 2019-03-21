d7f06f3fad691c29ee55b05175eaa1d49d9a53a6
Tmktahu <tmktahu@gmail.com>
2019-03-21 15:57:35 -0700

    Comment things

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 // this is the main.js file
 //testing git
+
+// we are using https://github.com/exceljs/exceljs
+
+// ask the user for a target file
+// read the file, get the headers
+// list the columns they can choose from
+// itterate through all things using the column cell CONTENT as a key to deduplicate things
+// splice records together so we don't override data as we go
+// ask them where they want to save the new file (before or after??)
+// write the deduplicated file.
